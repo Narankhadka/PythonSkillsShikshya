e35a7f9fb959716205f5b1c6dc9c0df399ae172b
Naran khadka <khadkan855@gmail.com>
2026-01-16 07:47:44 +0545

    learn something new

--- PythonFunction/PassbyValueAndPassByReference.py
+++ PythonFunction/PassbyValueAndPassByReference.py
@@ -1,8 +1,11 @@
 
 def testReference(arg):
     print("ID inside the function is : " , id(arg))
+    arg=arg+1
+    print("New object after increment",arg, id(arg))
 
-
-var="Hi Naran how are you doing"
+var=10
+#var="Hi Naran how are you doing"
 print("Id Before Passing: ",id(var))
 testReference(var)
+print("Value after function call",var)
